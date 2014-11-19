44556050fe8a71704c1ca36ba8471e5a73ce11b6
William <william@wjwjr.com>
2014-11-19 11:02:09 -0500

    turned off mouseover trigger

---
+++ app.js
@@ -0,0 +1,16 @@
+
+    function alertMe(){
+        alert("The link has been clicked!");
+    }
+
+    function changeColor(elem) {
+        elem.style.border = "2px solid #ffc52c";
+        alert("The color has changed to yellow");
+
+    }
+
+    function changeBack(elem) {
+        elem.style.border = "";
+        alert("The color has changed back");
+
+    }
